9e4043d344e30fc1ec563d448a3370af9150924e
promach <feiphung@hotmail.com>
2021-04-16 23:20:08 +0800

    Modified for the correct FPGA device naming

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -e
 yosys run_yosys.ys
 edif2ngd ddr3_memory_controller.edif
-ngdbuild ddr3_memory_controller -uc ddr3_memory_controller.ucf -p xc6slx9csg324-3
+ngdbuild ddr3_memory_controller -uc ddr3_memory_controller.ucf -p xc6slx16-3ftg256
 map -w ddr3_memory_controller
 par -w ddr3_memory_controller.ncd ddr3_memory_controller_par.ncd
 bitgen -w ddr3_memory_controller_par.ncd -g StartupClk:JTAGClk
